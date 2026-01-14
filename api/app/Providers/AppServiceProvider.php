00875fffdd79375f92ab317d11371060a06d0361
jaredshami <jaredshami1979@gmail.com>
2026-01-14 15:24:25 -0700

    Register SendGrid mailer transport in AppServiceProvider

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mailer\Bridge\Sendgrid\Transport as SendgridTransport;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Register SendGrid transport
+        \Illuminate\Support\Facades\Mail::extend('sendgrid', function ($app) {
+            $apiKey = $app['config']['services']['sendgrid']['secret'] ?? env('SENDGRID_API_KEY');
+            
+            return new \Symfony\Component\Mailer\Transport\SendgridApiTransport(
+                $apiKey,
+                $app['mailer.transport']
+            );
+        });
     }
 }
