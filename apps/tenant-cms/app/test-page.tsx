1480d9a72f8728c7cc4c98523737608ce8de495a
AutoDealerCloud <admin@autodealercloud.com>
2026-01-27 14:04:18 -0700

    Add test page to debug routing

---
+++ apps/tenant-cms/app/test-page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic'
+
+export default function TestPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Test Page - This should render</h1>
+      <p>If you see this, routing is working correctly.</p>
+    </div>
+  )
+}
